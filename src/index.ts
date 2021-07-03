5bae7f0a80732544dc9a193aabd65afe6a6141aa
yunitjung <tjungyuni@gmail.com>
2021-07-03 16:05:44 +0700

    add migration up

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import microConfig from './mikro-orm.config';
 const main = async () => {
     const orm =  await MikroORM.init(microConfig);
 
+    // run migration automatically
+    await orm.getMigrator().up();
+
     const post = orm.em.create(Post, {title: 'first post'});
     await orm.em.persistAndFlush(post);
 };

8e61f51ee549e4507ab1fd2a6349982ffa96de07
yunitjung <tjungyuni@gmail.com>
2021-07-05 10:38:40 +0700

    add try catch to check user exist

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -70,10 +70,40 @@ UserResponse = __decorate([
 let UserResolver = class UserResolver {
     register(options, { em }) {
         return __awaiter(this, void 0, void 0, function* () {
+            if (options.username.length <= 2) {
+                return {
+                    errors: [{
+                            field: 'username',
+                            message: 'length must be greater than 2'
+                        }]
+                };
+            }
+            if (options.password.length <= 2) {
+                return {
+                    errors: [{
+                            field: 'password',
+                            message: 'length must be greater than 2'
+                        }]
+                };
+            }
             const hashedPassword = yield argon2_1.default.hash(options.password);
             const user = em.create(User_1.User, { username: options.username, password: hashedPassword });
-            yield em.persistAndFlush(user);
-            return user;
+            try {
+                yield em.persistAndFlush(user);
+            }
+            catch (error) {
+                if (error.code === '23505' || error.detail.includes('already exists')) {
+                    return {
+                        errors: [
+                            {
+                                field: "username",
+                                message: "username already taken"
+                            }
+                        ]
+                    };
+                }
+            }
+            return { user };
         });
     }
     login(options, { em }) {
@@ -107,7 +137,7 @@ let UserResolver = class UserResolver {
     }
 };
 __decorate([
-    type_graphql_1.Mutation(() => User_1.User),
+    type_graphql_1.Mutation(() => UserResponse),
     __param(0, type_graphql_1.Arg('options')),
     __param(1, type_graphql_1.Ctx()),
     __metadata("design:type", Function),
